dea52caf319b85d0cc02a4789661b090cbc983cd
Andrew Mitty <cy.andrewmitty@gmail.com>
2018-12-07 00:55:59 +0300

    добавиÐил фронт логику

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ gulp.task('browserSync', function() {
     server: {
       baseDir: "./public_html/"
     },
-    port: 8080,
+    port: 8082,
     open: true,
     notify: false
   });
--- public_html/js/app.js
+++ public_html/js/app.js
@@ -3,7 +3,7 @@ contentArea = document.querySelector('.content');
 
 serviceSiteDev = document.querySelector('#site-development');
 serviceSiteDev.addEventListener('click', function () {
-  siteTypeBlock = document.querySelector('.cms');
+  siteTypeBlock = document.querySelector('.site-development-block');
     if (siteTypeBlock.style.display === 'block') {
       siteTypeBlock.style.display = 'none';
     } else {
@@ -29,4 +29,54 @@ wordpressType.addEventListener('click', function () {
   } else {
     parent.style.display = 'block';
   }
+});
+
+generatePDFButton = document.querySelector('.generate-pdf');
+generatePDFButton.addEventListener('click', function () {
+  //Проверка выбрана ли хоть одна услуга
+  serviceSelected = false;
+  servicesInputs = document.querySelectorAll('.services-menu input');
+  servicesInputs.forEach(function (service) {
+    if (service.checked) {
+      serviceSelected = true;
+    }
+  });
+  if (!serviceSelected) return;
+
+  //preparing Data
+  let kpData = new Object();
+  let companyName = (document.querySelector('#company-name')).value;
+
+  let siteDevelopmentSelected = (document.querySelector('#site-development')).checked ? true : false;
+  let seoPromotionSelected = (document.querySelector('#seo-promotion')).checked ? true : false;
+  let contextualAdvertisingSelected = (document.querySelector('#contextual-advertising')).checked ? true : false;
+
+  // Если выбрана услуга разработка сайта
+  if (siteDevelopmentSelected) {
+    let serviceName = document.querySelector('#site-development');
+    let siteTypeSelected =  document.querySelector('.site-type .item input:checked');
+    kpData.serviceType = serviceName.dataset.serviceName;
+    kpData.siteType = siteTypeSelected.dataset.typeName;
+
+    //Выбор системы управления сайтом (CMS)
+    let bitrixCMSSelected = (document.querySelector('#bitrix')).checked ? true : false;
+    let wordpressCMSSelected = (document.querySelector('#wordpress')).checked ? true : false;
+
+    if (bitrixCMSSelected) {
+      cmsText = (document.querySelector('#bitrix-text-area')).value;
+      cmsPrice = (document.querySelector('#bitrix-price-area')).value;
+
+      kpData.cmsBitrix = {text: cmsText, price: cmsPrice};
+    }
+
+    if (wordpressCMSSelected) {
+      cmsText = (document.querySelector('#wordpress-text-area')).value;
+      cmsPrice = (document.querySelector('#wordpress-price-area')).value;
+
+      kpData.cmsWordpress = {text: cmsText, price: cmsPrice};
+    }
+
+  }
+
+  console.log(kpData);
 });
